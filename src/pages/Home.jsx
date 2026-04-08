7a66bc9e8d9fa03c71dd01728e89eb5b87e579f7
Ali Hamza <alihamzaahmad680@gmail.com>
2026-04-08 21:49:38 +0500

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,101 @@
+// import React, {useEffect, useState} from 'react'
+// import appwriteService from "../appwrite/config";
+// import {Container, PostCard} from '../components'
+
+// function Home() {
+//     const [posts, setPosts] = useState([])
+
+//     useEffect(() => {
+//         appwriteService.getPosts().then((posts) => {
+//             if (posts) {
+//                 setPosts(posts.documents)
+//             }
+//         })
+//     }, [])
+  
+//     if (posts.length === 0) {
+//         return (
+//             <div className="w-full py-8 mt-4 text-center">
+//                 <Container>
+//                     <div className="flex flex-wrap">
+//                         <div className="p-2 w-full">
+//                             <h1 className="text-2xl font-bold hover:text-gray-500">
+//                                 Login to read posts
+//                             </h1>
+//                         </div>
+//                     </div>
+//                 </Container>
+//             </div>
+//         )
+//     }
+//     return (
+//         <div className='w-full py-8'>
+//             <Container>
+//                 <div className='flex flex-wrap'>
+//                     {posts.map((post) => (
+//                         <div key={post.$id} className='p-2 w-1/4'>
+//                             <PostCard {...post} />
+//                         </div>
+//                     ))}
+//                 </div>
+//             </Container>
+//         </div>
+//     )
+// }
+
+// export default Home
+import React, { useEffect, useState } from 'react'
+import appwriteService from "../appwrite/config";
+import { Container, PostCard } from '../components'
+
+function Home() {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await appwriteService.getPosts();
+
+            // 🔥 SAFE CHECK
+            if (res && res.documents) {
+                setPosts(res.documents)
+            } else {
+                setPosts([]) // fallback
+            }
+        }
+
+        fetchPosts()
+    }, [])
+
+    // 🔥 SAFE CHECK (no undefined error)
+    if (!posts || posts.length === 0) {
+        return (
+            <div className="w-full py-8 mt-4 text-center">
+                <Container>
+                    <div className="flex flex-wrap">
+                        <div className="p-2 w-full">
+                            <h1 className="text-2xl font-bold hover:text-gray-500">
+                                Login to read posts
+                            </h1>
+                        </div>
+                    </div>
+                </Container>
+            </div>
+        )
+    }
+
+    return (
+        <div className='w-full py-8'>
+            <Container>
+                <div className='flex flex-wrap'>
+                    {posts.map((post) => (
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard {...post} />
+                        </div>
+                    ))}
+                </div>
+            </Container>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/pages/Login.jsx
@@ -0,0 +1,23 @@
+// import React from 'react'
+// import { Login as loginComponent } from '../components'
+// function Login() {
+//   return (
+//     <div className='py-8'>
+//         <loginComponent />
+//     </div>
+//   )
+// }
+
+// export default Login
+import React from 'react'
+import { Login as LoginComponent } from '../components'
+
+function Login() {
+  return (
+    <div className='py-8'>
+        <LoginComponent />
+    </div>
+  )
+}
+
+export default Login
